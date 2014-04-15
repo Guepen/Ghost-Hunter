a11d0903b7166cac96d76317cee13f267e4a1afa
th222fa <th222fa@student.lnu.se>
2014-04-15 21:54:12 +0200

    spelaren är nu rörlig

--- script/Game.js
+++ script/Game.js
@@ -15,18 +15,18 @@ var Game = {
     rendering: false,
 
     //låter bilderna laddas innan spelet startar så de inte saknas när de skall användas
-    pictureLoader: function(){
+    pictureLoader: function () {
         Game.gameSprite = new Image();
         Game.gameSprite.src = "pictures/gameSprite.png";
 
         //när bilderna är laddade anropas funktionen renderBackground som ritar ut bakgrunden
-        Game.gameSprite.onload = function(){
+        Game.gameSprite.onload = function () {
             Game.renderBackground();
 
         }
     },
 
-    renderBackground: function(){
+    renderBackground: function () {
         Game.backgroundCanvas = document.getElementById("backgroundCanvas").getContext("2d");
         var srcX = 0; //x-pixeln i spriten som bakgrunden börjar på
         var srcY = 0; //y-pixeln i spriten som bakgrunden börjar på
@@ -39,35 +39,40 @@ var Game = {
         Game.init();
     },
 
-    init: function(){
+    init: function () {
         Game.playerCanvas = document.getElementById("playerCanvas").getContext("2d");
         Game.playerCanvas.fillStyle = "blue";
 
         Game.player = new Player();
-        console.log(Game.player);
+        //console.log(Game.player);
         //alert("Hello world");
+        document.addEventListener('keydown', keyDown, false);
+        document.addEventListener("keyup", keyUp, false);
+
         startLoop();
     }
 };
+
 
 function Player(){
     this.x = 400;
     this.height = 50;
     this.y = 450;
     this.width = 50;
-    this.speed = 3;
+    this.speed = 5;
     this.moveLeft = false;
     this.moveRight = false;
 }
 
 Player.prototype.render = function(){
-    //alert(this.y);
-    Game.playerCanvas.clearRect(0, 0, Game.width, Game.height);
+    //console.log("loop");
+    Game.playerCanvas.clearRect(0, 0, 800, 500);
     this.checkDirection();
     Game.playerCanvas.fillRect(this.x, this.y, this.width, this.height);
 };
 
 Player.prototype.checkDirection = function(){
+    console.log("hej");
     if(this.moveLeft){
         this.x -= this.speed;
     }
@@ -77,6 +82,32 @@ Player.prototype.checkDirection = function(){
     }
 };
 
+function keyDown(e){
+    var keyId = e.keyCode || e.which;
+    if(keyId === 37 || keyId === 65){ //flyttar spelaren åt vänster
+        e.preventDefault();
+        Game.player.moveLeft = true;
+    }
+
+    if(keyId === 39 || keyId === 68){ //flyttar spelaren åt höger
+        e.preventDefault();
+        Game.player.moveRight = true;
+    }
+}
+
+function keyUp(e){
+    var keyId = e.keyCode || e.which;
+    if(keyId === 37 || 65){ //om spelaren inte flyttas till vänster
+        e.preventDefault();
+        Game.player.moveLeft = false;
+    }
+
+    if(keyId === 39 || 68){ //om spelaren inte flyttas till höger
+        e.preventDefault();
+        Game.player.moveRight = false;
+    }
+}
+
 //"shim" av Paul Irish som används för bättre aniamtioner
 var animFrame = window.requestAnimationFrame ||
                 window.webkitRequestAnimationFrame ||
@@ -85,7 +116,7 @@ var animFrame = window.requestAnimationFrame ||
                 window.msRequestAnimationFrame ||
                 null;
 
-//använder animframe för att skapa loopen
+//funktonen loop kallar på sig själv med hjälp av aninframe
 function loop(){
     if(Game.rendering){
         Game.player.render();
