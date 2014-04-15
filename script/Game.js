91d9c596ce7d91e67a066256ccf673e879f419e4
th222fa <th222fa@student.lnu.se>
2014-04-15 20:48:32 +0200

    Implementerat spelaren

--- script/Game.js
+++ script/Game.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// objektet Game kapslar in koden så att bl.a. globala variabler undviks
+// objektet Game kapslar in kod som behöver köras innan spelet kan starta
 var Game = {
     player: null,
     bullet: null,
@@ -41,8 +41,39 @@ var Game = {
 
     init: function(){
         Game.playerCanvas = document.getElementById("playerCanvas").getContext("2d");
+        Game.playerCanvas.fillStyle = "blue";
+
+        Game.player = new Player();
+        console.log(Game.player);
         //alert("Hello world");
         startLoop();
+    }
+};
+
+function Player(){
+    this.x = 400;
+    this.height = 50;
+    this.y = 450;
+    this.width = 50;
+    this.speed = 3;
+    this.moveLeft = false;
+    this.moveRight = false;
+}
+
+Player.prototype.render = function(){
+    //alert(this.y);
+    Game.playerCanvas.clearRect(0, 0, Game.width, Game.height);
+    this.checkDirection();
+    Game.playerCanvas.fillRect(this.x, this.y, this.width, this.height);
+};
+
+Player.prototype.checkDirection = function(){
+    if(this.moveLeft){
+        this.x -= this.speed;
+    }
+
+    if(this.moveRight){
+        this.x += this.speed;
     }
 };
 
@@ -57,6 +88,7 @@ var animFrame = window.requestAnimationFrame ||
 //använder animframe för att skapa loopen
 function loop(){
     if(Game.rendering){
+        Game.player.render();
         animFrame(loop);
     }
 }
